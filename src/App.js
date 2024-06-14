35e82958d56b11cf86dbc99047a00f8b53a19d8b
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-06-14 16:50:35 +0100

    Installing React Router and Initial Set Up

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, 
+  createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements);
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router= {appRouter}>
+      <Route>
+
+        
+      </Route>
+
+
+    </RouterProvider>
+    
+    
+    
   );
 }
 
